9c169300760e6f6e5984161d86b3235309ce5b17
omkarh2308 <omkar2308@gmail.com>
2020-06-08 15:35:18 +0530

    Changes in AccountsRepositoryInMemory

--- challenge/challenge/src/main/java/com/db/awmd/challenge/repository/AccountsRepositoryInMemory.java
+++ challenge/challenge/src/main/java/com/db/awmd/challenge/repository/AccountsRepositoryInMemory.java
@@ -29,5 +29,49 @@ public class AccountsRepositoryInMemory implements AccountsRepository {
   public void clearAccounts() {
     accounts.clear();
   }
+  
+  @Override
+  public void transitionAmount(String fromAccount, String toAccount, BigDecimal amount) {
+
+	  public synchronized void transitionAmount(String fromAccount, String toAccount, BigDecimal amount) {
+	  if (getAccount(fromAccount) != null && getAccount(toAccount) != null) {
+			Account acc_from = getAccount(fromAccount);
+			boolean deb_flag = debitedFromAccount(acc_from, amount);
+			if (deb_flag) {
+				Account acc_to = getAccount(toAccount);
+				boolean cre_Flag = creditedToAccount(acc_to, amount);
+				if (cre_Flag) {
+					sendNotification(acc_from, acc_to, amount);
+				} else {
+					throw new InsufficientBalanceException(" Insufficient balance ....!!!");
+				}
+			}
+		}
+	}
+	public boolean debitedFromAccount(Account account, BigDecimal amount) {
+		if (amount.doubleValue() < account.getBalance().doubleValue()) {
+			account.setBalance(account.getBalance().subtract(amount));
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public boolean creditedToAccount(Account account, BigDecimal amount) {
+		account.setBalance(account.getBalance().add(amount));
+		return true;
+	}
+	public void sendNotification(Account accountFrom, Account accountTo, BigDecimal amount) {
+		try {
+			String acc_from = amount + " has been transfered to " + accountTo.getAccountId()
+					+ " & your Current Balance is : " + accountFrom.getBalance();
+			String acc_to = amount + " has been transfered from " + accountFrom.getAccountId()
+					+ " & your Current Balance is : " + accountTo.getBalance();
+			notificationService.notifyAboutTransfer(accountFrom, acc_from);
+			notificationService.notifyAboutTransfer(accountTo, acc_to);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+  
 
 }
